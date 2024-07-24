53fc21274bab69c9e0542fc11a731beb0b92bad3
Tsiresgrammeur <tsiresgrammeur@gmail.com>
2024-07-24 10:26:47 +0300

    creating sidebar

---
+++ src/template/Navbar/SidebarData.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { SiGoogleclassroom } from 'react-icons/si';
+import { GiTeacher } from 'react-icons/gi';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+
+export const SidebarData = [
+  {
+    title: 'Dashboard',
+    path: '/',
+    icon: <AiIcons.AiFillHome />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Users',
+    icon: <IoIcons.IoIosPaper />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Photos',
+    icon: <SiGoogleclassroom/>,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Configuration',
+    icon: <IoIcons.IoMdPeople />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Enseignement',
+    icon: <GiTeacher />,
+    cName: 'nav-text'
+  },
+];

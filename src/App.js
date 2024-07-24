8bfcd764f4b9b5de59fb76c151616b90cbf8d48d
Tsiresgrammeur <tsiresgrammeur@gmail.com>
2024-07-24 10:26:39 +0300

    creating sidebar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route,useNavigate, Link } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import Accueil from './accueil/Accueil';
+import {  useProSidebar } from "react-pro-sidebar"
+import Navbar from './template/Navbar/Navbar';
+
 
 function App() {
+  const { collapseSidebar } = useProSidebar();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+        <main>
+          <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_RIGHT} />
+          <BrowserRouter>
+        <Navbar />
+            <Routes>
+              <Route index element={<Accueil />} />
+              <Route path="accueil" element={<Accueil />} />
+            </Routes>
+          </BrowserRouter>
+        </main>
+      </div>
   );
 }
 
